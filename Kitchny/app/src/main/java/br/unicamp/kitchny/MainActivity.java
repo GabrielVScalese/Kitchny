bbd8bd034ea8a43dc59631d9cfc208379bf2ef1b
Gabriel Scalese <cc19171@g.unicamp.br>
2020-09-09 12:35:29 -0300

    Updated

--- Kitchny/app/src/main/java/br/unicamp/kitchny/MainActivity.java
+++ Kitchny/app/src/main/java/br/unicamp/kitchny/MainActivity.java
@@ -2,13 +2,31 @@ package br.unicamp.kitchny;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCriarConta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        btnCriarConta = findViewById(R.id.btnCriarConta);
+
+        btnCriarConta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,TelaDeCriarConta.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ Kitchny/app/src/main/java/br/unicamp/kitchny/TelaDeCriarConta.java
@@ -0,0 +1,70 @@
+package br.unicamp.kitchny;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.strictmode.CleartextNetworkViolation;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.UUID;
+
+public class TelaDeCriarConta extends AppCompatActivity {
+
+    EditText etNome;
+    EditText etEmail;
+    EditText etSenha;
+    EditText etConfirmarSenha;
+    Button btnCriar;
+    Usuario usuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_de_criar_conta);
+        getSupportActionBar().hide();
+
+        etNome = findViewById(R.id.etNome);
+        etEmail = findViewById(R.id.etEmail);
+        etSenha = findViewById(R.id.etSenha);
+        etConfirmarSenha = findViewById(R.id.etConfirmarSenha);
+        btnCriar = findViewById(R.id.btnCriarConta);
+
+        btnCriar.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ShowToast")
+            @Override
+            public void onClick(View view) {
+                usuario = new Usuario(etEmail.getText().toString(), etNome.getText().toString(), etSenha.getText().toString());
+                MyTask task = new MyTask();
+                task.execute("http://192.168.0.28:3000/api/insertUsuario");
+            }
+        });
+    }
+
+    private class MyTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            ClienteWS.postObjeto(usuario, Integer.class, params[0]);
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+        }
+    }
+}
